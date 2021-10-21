3bb1a432c60c20dc612b0e2fa15e7d0cbeba98d6
Marko Bakovic <markobakovic95@gmail.com>
2021-10-21 15:01:21 +0200

    Update template

--- templates/hello-world/example.ts
+++ templates/hello-world/example.ts
@@ -2,7 +2,7 @@ import {
 	ActionFn,
 	Context,
 	Event
-} from 'tenderly-actions'
+} from '@tenderly/actions'
 
 export const helloWorldFn: ActionFn = async (context: Context, event: Event) => {
 	console.log("Hello World!");
