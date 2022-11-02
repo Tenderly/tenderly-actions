10d2522a83ef5ba26c43aa76b167444852f02203
Veljko Matic <veljko@tenderly.co>
2022-11-02 13:29:35 +0100

    Add support for gateways

---
+++ packages/actions-test/src/gateways.ts
@@ -0,0 +1,14 @@
+import {Gateways, Network} from "@tenderly/actions";
+
+export class TestGateways implements Gateways {
+    private gatewayConfig: Map<string, any>;
+
+    constructor() {
+        this.gatewayConfig = new Map<string, any>();
+    }
+
+    getGateway(network: Network, name?: string | undefined): string {
+        const gatewayConfig = this.gatewayConfig.get(name ?? "")
+        return `https://${network}.gateway.tenderly-test.co/${gatewayConfig.accessKey}`
+    }
+}
