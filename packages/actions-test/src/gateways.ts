bb42389bfb46cfcfd8efa498f27e34dc8924fb98
Dragan Obradovic <g4ndr4@gmail.com>
2023-04-12 13:54:25 +0200

    Bumping `@tenderly/actions-test` to `0.2.0`.

--- packages/actions-test/src/gateways.ts
+++ packages/actions-test/src/gateways.ts
@@ -1,13 +1,22 @@
 import {Gateways, Network} from "@tenderly/actions";
+import {TestMetadata} from "./metadata";
 
 export class TestGateways implements Gateways {
     private gatewayConfig: Map<string, any>;
+    private metadata: TestMetadata;
 
     constructor() {
         this.gatewayConfig = new Map<string, any>();
+        this.metadata = new TestMetadata()
     }
 
-    getGateway(network: Network, name?: string | undefined): string {
+    getGateway(network?: Network, name?: string | undefined): string {
+        if (!network) {
+            const metadataNetwork = this.metadata.getNetwork()
+            if (!metadataNetwork) {
+                throw new Error("No network found for current execution!")
+            }
+        }
         const gatewayConfig = this.gatewayConfig.get(name ?? "")
         return `https://${network}.gateway.tenderly.co/${gatewayConfig?.accessKey}`
     }
