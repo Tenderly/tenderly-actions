4fb5f8a6656cefbb66dff8e1b7d77f0b73458a10
Djordje Mitrovic <djordje@tenderly.co>
2024-01-09 18:24:33 +0100

    missed eof

--- packages/actions/src/actions.ts
+++ packages/actions/src/actions.ts
@@ -252,5 +252,5 @@ export type GatewayNetwork = Extract<Network,
     Network.BASE |
     Network.BASE_GOERLI |
     Network.BOBA_GOERLI |
-    Network.HOLESKY> |
-    Network.ARBITRUM_SEPOLIA
+    Network.HOLESKY |
+    Network.ARBITRUM_SEPOLIA>
