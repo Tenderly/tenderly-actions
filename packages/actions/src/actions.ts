abea9cc331f0b4a652b90a7afdf1fc3e5e8ec907
nevio <nevio@tenderly.co>
2025-06-03 09:49:45 +0200

    Celo Mainnet

--- packages/actions/src/actions.ts
+++ packages/actions/src/actions.ts
@@ -293,7 +293,8 @@ export enum Network {
     ZIRCUIT = "zircuit",
     ZIRCUIT_GARFIELD_TESTNET = "zircuit-garfield-testnet",
     INJECTIVE_TESTNET = "injective-testnet",
-    CELO_ALFAJORES_TESTNET = "celo-alfajores-testnet"
+    CELO_ALFAJORES_TESTNET = "celo-alfajores-testnet",
+    CELO = "celo"
 }
 
 /**
@@ -379,4 +380,5 @@ export type GatewayNetwork = Extract<Network,
     Network.AF_NEXUS_TESTNET|
     Network.ZIRCUIT_GARFIELD_TESTNET|
     Network.ZIRCUIT|
-    Network.CELO_ALFAJORES_TESTNET>
+    Network.CELO_ALFAJORES_TESTNET|
+    Network.CELO>
