c67961b1bea353a0d35468e9fdbaaf1e3110ccc1
Abrahem.79 <184462822+abrahem79@users.noreply.github.com>
2025-11-06 17:39:50 +0300

    Revert "Add `OMEGA_13` network (`omega-13`) (#123)"
    This reverts commit 0a215a54b55175b776fbab501f0067456962110a.

--- packages/actions/src/networks.ts
+++ packages/actions/src/networks.ts
@@ -73,7 +73,6 @@ export enum Network {
     MOONBEAM = "moonbeam",
     MOONRIVER = "moonriver",
     MORPH = "morph",
-    OMEGA_13 = "omega-13",
     OPTIMISTIC = "optimistic",
     OPTIMISTIC_SEPOLIA = "optimistic-sepolia",
     PEAQ = "peaq",
