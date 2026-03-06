fc6d3203d750435cb50a77331a97bb068618ab99
GitHub Action <action@github.com>
2026-03-06 15:40:23 +0000

    chore: regenerate network files from networks.json

--- packages/actions/src/networks.ts
+++ packages/actions/src/networks.ts
@@ -110,6 +110,7 @@ export enum Network {
     TAC_SPB_TESTNET = "tac-spb-testnet",
     TAIKO = "taiko-mainnet",
     TAIKO_HOODI = "taiko-hoodi",
+    TEMPO = "tempo",
     TEMPO_MODERATO_TESTNET = "tempo-moderato-testnet",
     TEMPO_TESTNET = "tempo-testnet",
     UNICHAIN = "unichain",
