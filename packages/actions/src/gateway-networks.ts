26b5514f487e9e5b06fcb0c8786c9d14422ee8d6
Olivera Popovic <olivera@tenderly.co>
2026-03-12 16:15:01 +0100

    enable gateway for xlayers and tempo

--- packages/actions/src/gateway-networks.ts
+++ packages/actions/src/gateway-networks.ts
@@ -79,12 +79,15 @@ export type GatewayNetwork = Extract<Network,
     Network.SWELLCHAIN_SEPOLIA |
     Network.TAIKO |
     Network.TAIKO_HOODI |
+    Network.TEMPO |
     Network.TEMPO_MODERATO_TESTNET |
     Network.TEMPO_TESTNET |
     Network.UNICHAIN |
     Network.UNICHAIN_SEPOLIA |
     Network.WORLDCHAIN |
     Network.WORLDCHAIN_SEPOLIA |
+    Network.XLAYER |
+    Network.XLAYER_TESTNET |
     Network.ZIRCUIT |
     Network.ZIRCUIT_GARFIELD_TESTNET |
     Network.ZKSYNC |
